702feed166c419df7562166f78a2a1e7ef950648
Jimi Damon <jdamon@accesio.com>
2015-04-11 08:40:34 -0700

    Completed plot2

--- plot1.R
+++ plot1.R
@@ -2,11 +2,12 @@
 #
 #
 plot1 <- function(datafile="./household_power_consumption.txt" ) {
-    tmpdat <- read.csv(datafile,sep";")
+    tmpdat <- read.csv(datafile,sep=";")
     tmpdat$Date <- as.Date( tmpdat$Date, format="%d/%m/%Y" )
 
     filtered  <- tmpdat[tmpdat$Date == as.Date("2007-02-02") | tmpdat$Date == as.Date("2007-02-01"),]
     filtered$Global_active_power <- as.numeric(as.character(filtered$Global_active_power))
     hist(filtered$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
-
+    dev.copy(png,file="plot1.png")
+    dev.off()
 }
---
+++ plot2.R
@@ -0,0 +1,15 @@
+#
+#
+#
+plot2 <- function(datafile="./household_power_consumption.txt" ) {
+    tmpdat <- read.csv(datafile,sep=";")
+    ## tmpdat$Date <- as.Date( tmpdat$Date, format="%d/%m/%Y" )
+    tmpdat$ntime <- strptime( paste(tmpdat$Date,tmpdat$Time), format="%d/%m/%Y %H:%M:%S")
+    startdate = strptime("2007-02-01",format="%Y-%m-%d")
+    enddate  = strptime("2007-02-03",format="%Y-%m-%d")
+    filtered  <- tmpdat[tmpdat$ntime >= startdate & tmpdat$ntime < enddate,]
+    filtered$Global_active_power <- as.numeric(as.character(filtered$Global_active_power))
+    plot(filtered$ntime , filtered$Global_active_power, type="l", ylab="Global Active Power (kilowatts)",xlab="")
+    dev.copy(png,file="plot2.png")
+    dev.off()
+}
