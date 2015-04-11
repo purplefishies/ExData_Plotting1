a0c02efc0847e069731d8938bdb3dd422794356b
Jimi Damon <jdamon@accesio.com>
2015-04-11 09:26:50 -0700

    ENH: plot3 , but need to change o into line for legend

---
+++ plot3.R
@@ -0,0 +1,29 @@
+#
+#
+#
+plot3 <- function(datafile="./household_power_consumption.txt" ) {
+    tmpdat <- read.csv(datafile,sep=";")
+    par(bg="white")
+
+    tmpdat$ntime <- strptime( paste(tmpdat$Date,tmpdat$Time), format="%d/%m/%Y %H:%M:%S")
+    startdate = strptime("2007-02-01",format="%Y-%m-%d")
+    enddate  = strptime("2007-02-03",format="%Y-%m-%d")
+    filtered  <- tmpdat[tmpdat$ntime >= startdate & tmpdat$ntime < enddate,]
+    filtered$Global_active_power <- as.numeric(as.character(filtered$Global_active_power))
+    filtered$Sub_metering_1 <- as.numeric(as.character(filtered$Sub_metering_1))
+    filtered$Sub_metering_2 <- as.numeric(as.character(filtered$Sub_metering_2))
+    filtered$Sub_metering_3 <- as.numeric(as.character(filtered$Sub_metering_3))
+
+    plot(filtered$ntime , filtered$Sub_metering_1, type="l", col="black", ylab="Global Active Power (kilowatts)",xlab="" )
+    
+    lines(filtered$ntime, filtered$Sub_metering_2, col="red", type="l")
+    lines(filtered$ntime, filtered$Sub_metering_3, col="blue", type="l")
+
+    # Now add the axis
+
+    legend("topright", pch = 1, col = c("black", "red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+    
+    dev.copy(png,file="plot3.png")
+    dev.off()
+}
